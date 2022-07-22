c199cc987f551e4a2f0139da57c65593afc69185
skismile <sk.ismile75@gmail.com>
2022-07-22 11:06:34 +0530

    add

--- project/src/index.js
+++ project/src/index.js
@@ -3,26 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react'
-import { AppcontextProvider } from './context/appcontext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-
-
-   <BrowserRouter>
-
-   <ChakraProvider>
-    <AppcontextProvider>
-
+  <React.StrictMode>
     <App />
-    </AppcontextProvider>
-   </ChakraProvider>
-   </BrowserRouter>
-  
-  
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
